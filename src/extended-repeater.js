16b79eb8641e5858b4714d95abfec91de44fdcff
Yuliya Mironova <rabkevich15@gmail.com>
2021-09-21 23:52:35 +0200

    repeter done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,23 +15,32 @@ import { NotImplementedError } from "../extensions/index.js";
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
+
 export default function repeater(str, options) {
+  let {
+    repeatTimes = 0,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 0,
+    additionSeparator = "|",
+  } = options;
   let unswer = "";
-  if (
-    options.repeatTimes == undefined ||
-    options.additionRepeatTimes == undefined
-  ) {
-    unswer = str;
-    return unswer;
-  } else {
-    let str = str.toString();
-    let addition = "" + options.addition;
-    unswer;
-  }
+  str = "" + str;
+  addition = "" + addition;
+  let unswerAddition = repitAdd(
+    addition,
+    additionSeparator,
+    additionRepeatTimes
+  );
+  let newStr = str + unswerAddition;
+  unswer = repitAdd(newStr, separator, repeatTimes);
+  return unswer;
 }
-//console.log(repeater("la", { repeatTimes: 3 }));
 
 function repitAdd(str, sep, times) {
+  if (times === 0) {
+    times = 1;
+  }
   let repited = "";
   let repSep = "" + str + sep;
   let symbolslice = (str.length + sep.length) * times - sep.length;
@@ -39,8 +48,4 @@ function repitAdd(str, sep, times) {
   repited = repited.slice(0, symbolslice);
   return repited;
 }
-console.log(repitAdd("cat", "!!", 2));
-
-//repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS',
-//additionRepeatTimes: 3, additionSeparator: '00' })
-//=> 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+console.log(repeater("la", { repeatTimes: 3 })); //TESTstrADD!
