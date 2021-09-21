d39d1d3099667112af913c07407b33e4dcfe7718
Yuliya Mironova <rabkevich15@gmail.com>
2021-09-21 23:03:52 +0200

    common-chart

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  let s1Arr = s1.split("");
+  let s2Arr = s2.split("");
+  let num = 0;
+  let idx;
+  for (let i = 0; i < s1Arr.length; i++) {
+    if (s2Arr.includes(s1Arr[i])) {
+      num++;
+      idx = s2Arr.indexOf(s1Arr[i]);
+      s2Arr.splice(idx, 1);
+    }
+  }
+
+  return num;
+  //console.log(s1Arr);
 }
+console.log(getCommonCharacterCount("aaa", "aa"));
+//"mocha -r esm"
